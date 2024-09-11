3ca168aa77fdbb643d94b711a7008936b5930845
dukunuu <erdeneculuundlgn@gmail.com>
2024-09-10 20:51:01 -0400

    updated Lazy pavckages

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,7 +19,7 @@ return {
       local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       lspconfig.lua_ls.setup({ capabilities = capabilities })
-      lspconfig.tsserver.setup({ capabilities = capabilities })
+      lspconfig.ts_ls.setup({ capabilities = capabilities })
       lspconfig.volar.setup({ capabilities = capabilities })
       lspconfig.tailwindcss.setup({ capabilities = capabilities })
       lspconfig.pyright.setup({ capabilities = capabilities })
